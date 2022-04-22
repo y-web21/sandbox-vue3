4ff27cc5345ce84791d1b450f9e0b6ad2e274e1e
y-web21 <51986098+y-web21@users.noreply.github.com>
2022-04-22 19:19:22 +0900

    v-for challenge

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
   <header class="header">
-    <img
-      src="/images/logo.svg"
-      alt="">
+    <img src="/images/logo.svg" alt="">
     <h1>Vue.js ハンズオン</h1>
   </header>
   <main class="main">
     <template v-for="item in items" :key="item.id">
-    <div class="item">
-      <div class="thumbnail">
-        <img
-          :src="item.image"
-          alt="">
+      <div class="item">
+        <div class="thumbnail">
+          <img :src="item.image" alt="">
+        </div>
+        <div class="description">
+          <h2>{{ item.name }}</h2>
+          <p>{{ item.description }}</p>
+          <span>¥<span class="price">{{ item.price }}</span></span>
+        </div>
       </div>
-      <div class="description">
-        <h2>{{ item.name }}</h2>
-        <p>{{ item.description }}</p>
-        <span>¥<span class="price">{{ item.price }}</span></span>
-      </div>
-    </div>
     </template>
   </main>
 
+<div id="app">
+  <!-- :key の index 指定は、並び替えなどで不具合があるため非推奨とのこと -->
+  <!-- だが、:key の指定がないとエラーを吐くため必須だと思われる。 []の配列形式だと、indexでも渡しておかないと動かないのでとりあえず渡す -->
+  <ul>
+    <li v-for="(page, index) in pages" :key="index">{{index}}, {{ page }}</li>
+  </ul>
+  <!-- key value (index) の取り出し -->
+  <!-- 変数の名前は自由だが、value, key, index が固定のようだ。 -->
+  <ul>
+    <li v-for="(value, key, yandex) in dicts" :key="yandex">{{yandex}}, {{key}}, {{ value }}</li>
+  </ul>
+</div>
 </template>
 
 <script>
@@ -30,39 +38,49 @@ export default {
   data() {
     return {
       items: [
-      {
-        id: 1,
-        name: 'アボカドディップバケッ！',
-        description:
-          '刻んだ野菜をディップに優しいアボカドに味のソースと混ぜて。こんがり焼いたバゲットとお召し上がれ。',
-        price: 48000,
-        image: '/images/item1.jpg'
-      },
-      {
-        id: 2,
-        name: '小麦粉',
-        description:
-          '焼きました。夢。',
-        price: 20,
-        image: '/images/item2.jpg'
-      },
-      {
-        id: 3,
-        name: 'HOP WTR',
-        description:
-          '生まれはロスの炭酸水。CO2!CO2!',
-        price: 320,
-        image: '/images/item3.jpg'
-      },
-      {
-        id: 4,
-        name: 'チーレンチライ',
-        description:
-          'イタリア大好きなみんなをだっぷりかけたアツアツです。フライの一品フレンチ。',
-        price: 670,
-        image: '/images/item4.jpg'
+        {
+          id: 1,
+          name: 'アボカドディップバケッ！',
+          description:
+            '刻んだ野菜をディップに優しいアボカドに味のソースと混ぜて。こんがり焼いたバゲットとお召し上がれ。',
+          price: 48000,
+          image: '/images/item1.jpg'
+        },
+        {
+          id: 2,
+          name: '小麦粉',
+          description:
+            '焼きました。夢。',
+          price: 20,
+          image: '/images/item2.jpg'
+        },
+        {
+          id: 3,
+          name: 'HOP WTR',
+          description:
+            '生まれはロスの炭酸水。CO2!CO2!',
+          price: 320,
+          image: '/images/item3.jpg'
+        },
+        {
+          id: 4,
+          name: 'チーレンチライ',
+          description:
+            'イタリア大好きなみんなをだっぷりかけたアツアツです。フライの一品フレンチ。',
+          price: 670,
+          image: '/images/item4.jpg'
+        }
+      ],
+      pages: [
+        'page A',
+        'page B',
+        'page C',
+      ],
+      dicts: {
+        diA: 'dictA',
+        diB: 'dictB',
+        diC: 'dictC'
       }
-      ]
     }
   }
 }
@@ -90,13 +108,13 @@ body {
   margin-bottom: 40px;
 }
 
-.header > img {
+.header>img {
   width: 100px;
   height: 100px;
   margin-right: 20px;
 }
 
-.header > h1 {
+.header>h1 {
   font-size: 80px;
   font-weight: bold;
   line-height: 80px;
@@ -121,31 +139,31 @@ body {
   transform: scale(1.05);
 }
 
-.item > div.thumbnail > img {
+.item>div.thumbnail>img {
   width: 100%;
   height: calc(100%);
   object-fit: cover;
 }
 
-.item > div.description {
+.item>div.description {
   text-align: left;
   margin-top: 20px;
 }
 
-.item > div.description > p {
+.item>div.description>p {
   margin-top: 0px;
   margin-bottom: 0px;
   font-size: 18px;
   line-height: 25px;
 }
 
-.item > div.description > span {
+.item>div.description>span {
   display: block;
   margin-top: 10px;
   font-size: 20px;
 }
 
-.item > div.description > span > .price {
+.item>div.description>span>.price {
   font-size: 28px;
   font-weight: bold;
 }
